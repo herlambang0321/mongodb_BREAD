80c8819949e53c5e088638f7f482c8d668248167
herlambang0321 <herlambang88888888@gmail.com>
2021-10-20 17:11:31 +0700

    added get post

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = function (db) {
+  const collection = db.collection('bread');
+  router.get('/', async function (req, res, next) {
+    try {
+      const data = await collection.find({}).toArray();
+      res.json(data)
+    } catch (err) {
+      res.status(500).json({ err })
+    }
+  });
 
-module.exports = router;
+  router.post('/', async function (req, res, next) {
+    try {
+      const data = await collection.insertOne({ stringdata: req.body.stringdata, integerdata: parseInt(req.body.integerdata), floatdata: parseFloat(req.body.floatdata), datedata: req.body.datedata, booleandata: req.body.booleandata });
+      const item = await collection.findOne({_id: data.insertedId})
+      res.json(item)
+    } catch (err) {
+      res.status(500).json({ err })
+    }
+  });
+
+  return router;
+}
