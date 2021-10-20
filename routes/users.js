b750c06cf24a6345518ec8eac5646bbf40545afb
herlambang0321 <herlambang88888888@gmail.com>
2021-10-20 21:34:23 +0700

    added put

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const ObjectID = require('mongodb').ObjectId;
 
 module.exports = function (db) {
   const collection = db.collection('bread');
@@ -15,9 +16,21 @@ module.exports = function (db) {
   router.post('/', async function (req, res, next) {
     try {
       const data = await collection.insertOne({ stringdata: req.body.stringdata, integerdata: parseInt(req.body.integerdata), floatdata: parseFloat(req.body.floatdata), datedata: req.body.datedata, booleandata: req.body.booleandata });
-      const item = await collection.findOne({_id: data.insertedId})
+      const item = await collection.findOne({ _id: data.insertedId })
       res.json(item)
     } catch (err) {
+      res.status(500).json({ err })
+    }
+  });
+
+  router.put('/:id', async function (req, res, next) {
+    console.log(req.params.id)
+    try {
+      const data = await collection.updateOne({ _id: new ObjectID(req.params.id) }, { $set: { stringdata: req.body.stringdata, integerdata: parseInt(req.body.integerdata), floatdata: parseFloat(req.body.floatdata), datedata: req.body.datedata, booleandata: req.body.booleandata } }, { upsert: true });
+      // const item = await collection.findOne()
+      res.json(data)
+    } catch (err) {
+      console.log(err)
       res.status(500).json({ err })
     }
   });
