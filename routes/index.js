42191f3fa4d707da8fd049cdfc1448b2f8432420
herlambang0321 <herlambang88888888@gmail.com>
2021-11-02 16:44:35 +0700

    jquery Add & Delete

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'BREAD(Browse, Read, Edit, Add, Delete)' });
+// });
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BREAD(Browse, Read, Edit, Add, Delete)' });
+  res.render('jquery');
 });
 
 module.exports = router;
